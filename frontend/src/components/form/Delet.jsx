3febaf408dc11b47e2a4b5983425ed54b2f91309
Mateus178 <msuhet@gmail.com>
2023-10-11 11:55:04 -0300

    +modal Delet

--- frontend/src/components/form/Delet.jsx
+++ frontend/src/components/form/Delet.jsx
@@ -2,6 +2,7 @@ import React, { useCallback, useRef } from "react";
 import { usePocket } from "../../contexts/PocketContext";
 import * as Form from '@radix-ui/react-form';
 import {AiOutlineClose} from '@react-icons/all-files/ai/AiOutlineClose.esm'
+import "../../styles/modal.css";
 
 export default function Delet(props) {
     const {modalInfo} = props;
@@ -38,7 +39,7 @@ export default function Delet(props) {
 
     return( 
         <div className="modal__wrapper">
-            <div className="single__modal  dark:bg-dark-400 border-2 border-[#712cf9] dark:border-white">
+            <div className="simple__modal  dark:bg-dark-400 border-2 border-[#712cf9] dark:border-white">
             <span className="close__modal p-2">
                 <AiOutlineClose 
                     className="bg-[#712cf9] rounded-lg  text-white"
@@ -46,36 +47,44 @@ export default function Delet(props) {
                     setShowDelet(false)
                 }}/> 
             </span>
-            <div className="p-6 max-w-sm mx-auto bg-gray-500 rounded-xl shadow-md flex items-center space-x-4">
-                <section>
-                <label className="block uppercase tracking-wide text-gray-700 text-center font-bold mb-2">
-                    Evento:
-                </label>
-                <input className="w-full bg-[#e2e8f0] dark:bg-dark-800 inline-flex h-10 appearance-none items-center justify-center rounded-md" defaultValue={props.modalInfo.event.title} ref={titleRef}></input>
-                <select  className="w-full dark:bg-dark-800 inline-flex h-[35px] appearance-none items-center justify-center rounded-md" placeholder={props.modalInfo.event.sala} ref={salaRef}>
-                    <option type="nome" value="Sala1" >Sala1 </option>
-                    <option type="nome" value="Sala2" >Sala2</option>
-                    <option type="nome" value="Sala3" >Sala3</option>
-                </select>
-                </section>
-            </div>
-                <Form.Root onSubmit={handleOnSubmit}>
-                    <Form.Submit asChild>
-                        <button className='mt-3 dark:bg-dark-900 my-2 box-border w-full dark:text-white shadow-blackA7 dark:shadow-slate-500 hover:bg-mauve3 inline-flex h-[35px] items-center justify-center rounded-[4px] bg-white px-[15px] font-medium leading-none shadow-[0_2px_10px] focus:shadow-[0_0_0_2px] focus:shadow-black focus:outline-none' >
-                            Clique para Deletar
-                        </button>
-                    </Form.Submit>
+            <Form.Root className="" >
+                <Form.Field className="FormField" name="atualiza">
+                <div style={{ display: 'flex', alignItems: 'baseline', justifyContent: 'space-between' }}>
+                    <Form.Label className="FormLabel" >Titulo</Form.Label>
+                </div>
+                <Form.Control asChild>
+                    <input className="w-full bg-[#e2e8f0] dark:bg-dark-800 inline-flex h-10 appearance-none items-center justify-center rounded-md" defaultValue={props.modalInfo.event.title} ref={titleRef}/>
+                </Form.Control>
+                <div style={{ display: 'flex', alignItems: 'baseline', justifyContent: 'space-between' }}>
+                    <Form.Label className="FormLabel">Sala</Form.Label>
+                </div>
+                <Form.Control asChild>
+                    <input className="w-full bg-[#e2e8f0] dark:bg-dark-800 inline-flex h-10 appearance-none items-center justify-center rounded-md" defaultValue={props.modalInfo.event.extendedProps.sala} ref={salaRef}/>
+                </Form.Control>
+                </Form.Field>
+            </Form.Root>
+
+                <Form.Root className="" onSubmit={handleOnSubmit}>
+                    <Form.Field className="grid mb-[10px]" name="title">
+                        <div className="flex items-baseline justify-between">
+                            <Form.Submit asChild>
+                                <button className='mt-3 dark:bg-dark-900 my-2 box-border w-full dark:text-white shadow-blackA7 dark:shadow-slate-500 hover:bg-mauve3 inline-flex h-[35px] items-center justify-center rounded-[4px] bg-white px-[15px] font-medium leading-none shadow-[0_2px_10px] focus:shadow-[0_0_0_2px] focus:shadow-black focus:outline-none' >
+                                    Deletar
+                                </button>
+                            </Form.Submit>
+                            <button onClick={() => {
+                                up()
+                            }}className='mt-3 dark:bg-dark-900 my-2 box-border w-full dark:text-white shadow-blackA7 dark:shadow-slate-500 hover:bg-mauve3 inline-flex h-[35px] items-center justify-center rounded-[4px] bg-white px-[15px] font-medium leading-none shadow-[0_2px_10px] focus:shadow-[0_0_0_2px] focus:shadow-black focus:outline-none' >
+                                Atualizar
+                            </button>
+                            <button onClick={() => {
+                                setShowDelet(false)
+                            }}className='mt-3 dark:bg-dark-900 my-2 box-border w-full dark:text-white shadow-blackA7 dark:shadow-slate-500 hover:bg-mauve3 inline-flex h-[35px] items-center justify-center rounded-[4px] bg-white px-[15px] font-medium leading-none shadow-[0_2px_10px] focus:shadow-[0_0_0_2px] focus:shadow-black focus:outline-none' >
+                                Voltar
+                            </button>
+                        </div>
+                    </Form.Field>
                 </Form.Root>
-                <button onClick={() => {
-                    up()
-                }}className='mt-3 dark:bg-dark-900 my-2 box-border w-full dark:text-white shadow-blackA7 dark:shadow-slate-500 hover:bg-mauve3 inline-flex h-[35px] items-center justify-center rounded-[4px] bg-white px-[15px] font-medium leading-none shadow-[0_2px_10px] focus:shadow-[0_0_0_2px] focus:shadow-black focus:outline-none' >
-                    Clique para Atualizar
-                </button>
-                <button onClick={() => {
-                    setShowDelet(false)
-                }}className='mt-3 dark:bg-dark-900 my-2 box-border w-full dark:text-white shadow-blackA7 dark:shadow-slate-500 hover:bg-mauve3 inline-flex h-[35px] items-center justify-center rounded-[4px] bg-white px-[15px] font-medium leading-none shadow-[0_2px_10px] focus:shadow-[0_0_0_2px] focus:shadow-black focus:outline-none' >
-                    Clique para Voltar
-                </button>
             </div>
         </div>
     );
